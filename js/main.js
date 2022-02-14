cdca615f9a3251fec211326e5d0230e6c1c41319
kulakoff <anton.kulakoff@gmail.com>
2022-02-15 02:14:07 +0300

    added burger menu and play button function

--- js/main.js
+++ js/main.js
@@ -14,3 +14,26 @@ const swiper = new Swiper(".main-slider", {
     },
   },
 });
+
+let burger = document.querySelector(".menu-burger");
+let closeMenu = document.querySelector(".header__menu-close");
+let menuMobile = document.querySelector(".header");
+
+burger.addEventListener("click", () => {
+  menuMobile.style.display = "block";
+});
+closeMenu.addEventListener("click", () => {
+  menuMobile.style.display = "none";
+});
+const modalWindow = document.querySelector(".modal");
+const buttonModal = document.querySelector(".main__button-play");
+
+buttonModal.addEventListener("click", (e) => {
+  modalWindow.classList.add("active");
+});
+modalWindow.addEventListener("click", (e) => {
+  const isModal = e.target.closest(".modal__inner");
+  if (!isModal) {
+    modalWindow.classList.remove("active");
+  }
+});
